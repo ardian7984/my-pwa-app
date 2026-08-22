952c43a56b6b3e47725b12a35a31a5f25ca29d2e
ardian7984 <153589124+ardian7984@users.noreply.github.com>
2026-08-22 08:31:43 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    perubahan kode ke GitHub. Ini yang memicu HP pengguna membuang cache
    lama dan mengambil file baru.
    ===================================================================== */
-const CACHE_NAME = 'aplikasi-rt-cache-v17';
+const CACHE_NAME = 'aplikasi-rt-cache-v19';
 
 const APP_SHELL = [
   './index.html',
